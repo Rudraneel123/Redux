91afeb6ea20e8300cfc6d22ae593626197dfb9c7
Rudraneel Guha <101494979+Rudraneel123@users.noreply.github.com>
2024-06-30 23:29:43 +0530

    Counter App example using React-Redux-toolkit

---
+++ react-redux/src/Counter.jsx
@@ -0,0 +1,17 @@
+import {useSelector,useDispatch} from 'react-redux';
+import { increment,decrement } from './CounterSlice';
+
+const Counter=()=> {
+   const count=useSelector((state)=>state.counter.count);
+   const dispatch=useDispatch();
+
+  return (
+    <div>
+        <h1>Count :{count}</h1>
+        <button onClick={()=>dispatch(increment())}>+</button>
+        <button onClick={()=>dispatch(decrement())}>-</button>
+    </div>
+  )
+}
+
+export default Counter
